b08b18bcfdcf55699a37a943105efdce2dcb3fb9
laurant14 <58490200+laurant14@users.noreply.github.com>
2020-03-24 12:00:35 -0400

    Update LoadCommand.java

--- src/LoadCommand.java
+++ src/LoadCommand.java
@@ -1,7 +1,12 @@
-
+/**
+ * Class that implements Command using the execute and getName methods, which prompt the Document class to complete the specified action
+ */
 public class LoadCommand implements Command{
 	private Document doc;
 	
+	/**
+	 * Default constructor takes in doc as a paramter and sets the class variable doc to the parameter doc
+	 */
 	public LoadCommand(Document doc) {
 		this.doc=doc;
 	}
