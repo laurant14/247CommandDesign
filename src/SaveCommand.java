bf8e5946341292db6dd218712e7c9ddbcd1403eb
laurant14 <58490200+laurant14@users.noreply.github.com>
2020-03-24 12:01:41 -0400

    Update SaveCommand.java

--- src/SaveCommand.java
+++ src/SaveCommand.java
@@ -1,7 +1,14 @@
+/**
+ * Class that implements Command using the execute and getName methods, which prompt the Document class to complete the specified action
+ */
 
 public class SaveCommand implements Command{
 	private Document doc;
 	
+	
+	/**
+	 * Default constructor takes in doc as a paramter and sets the class variable doc to the parameter doc
+	 */
 	public SaveCommand(Document doc) {
 		this.doc=doc;
 	}
