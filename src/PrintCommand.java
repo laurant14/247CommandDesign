292900dd5732e9b65182e1336a05360e72f52c9a
laurant14 <58490200+laurant14@users.noreply.github.com>
2020-03-24 12:01:08 -0400

    Update PrintCommand.java

--- src/PrintCommand.java
+++ src/PrintCommand.java
@@ -1,8 +1,16 @@
+/**
+ * Class that implements Command using the execute and getName methods, which prompt the Document class to complete the specified action
+ */
+
 
 public class PrintCommand implements Command{
 	
 	private Document doc;
 	
+	
+	/**
+	 * Default constructor takes in doc as a paramter and sets the class variable doc to the parameter doc
+	 */
 	public PrintCommand(Document doc) {
 		this.doc=doc;
 	}
