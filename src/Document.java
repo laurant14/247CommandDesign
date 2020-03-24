5d61c70ec64504ba7c45afbeffab1e612635e708
laurant14 <58490200+laurant14@users.noreply.github.com>
2020-03-23 21:04:09 -0400

    Update Document.java

--- src/Document.java
+++ src/Document.java
@@ -1,23 +1,26 @@
+
+
 public class Document {
+	Command command;
 	
 	public Document(String name) {
 		System.out.println("Documented named " + name + "is created");//This needs to have the document input into it but howwww
 	}
 	
 	public void load() {
-		System.out.println("document " + name + "is loaded into view");
+		System.out.println("document " + command.getName() + "is loaded into view");
 	}
 	
 	public void spell() {
-		System.out.println("document " + doc + "is being chekced for spelling errors");
+		System.out.println("document " + command.getName() + "is being chekced for spelling errors");
 	}
 	
 	public void save() {
-		System.out.println("document " + name + "is being saved");
+		System.out.println("document " + command.getName() + "is being saved");
 	}
 	
 	public void print() {
-		System.out.println("document " +name + "is being saved");
+		System.out.println("document " +command.getName() + "is being saved");
 	}
 
 }
